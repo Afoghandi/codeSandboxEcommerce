ba3835eb3ed7810a26c624b08febace0560b2148
Afoghandi <58007934+Afoghandi@users.noreply.github.com>
2020-08-04 18:34:16 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+import Homepage from "./homepage.component";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Homepage />
+    </div>
+  );
+}
